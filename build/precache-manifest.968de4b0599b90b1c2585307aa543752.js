bc8ee82f6c5b798cff029dc158a6eb70c6066f0d
Conte <coding@Contes-MacBook-Pro.local>
2019-01-31 13:50:18 -0800

    cards updated to the office chars

---
+++ build/precache-manifest.968de4b0599b90b1c2585307aa543752.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "930d74ef5e0050825f33",
+    "url": "/static/js/main.930d74ef.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "930d74ef5e0050825f33",
+    "url": "/static/css/main.037286ca.chunk.css"
+  },
+  {
+    "revision": "776cbece187b157a268f1acb1efb782f",
+    "url": "/index.html"
+  }
+];
